9ac8402226f94c82840eef28936eeabf0ef7a054
hankun <ihankun@outlook.com>
2022-03-23 21:50:35 +0800

    结构调整

---
+++ commons/src/test/java/com/hankun/parent/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hankun.parent.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
